69ad5ed06a0662a2a52b9f0c694421718bf04a00
Astridstar <sohsum@gmail.com>
2023-03-09 15:50:32 +0000

    Need to set AIRFLOW_UID

--- 18-airflow/run.sh
+++ 18-airflow/run.sh
@@ -2,7 +2,8 @@
 
 curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.5.1/docker-compose.yaml'
 
+echo -e "AIRFLOW_UID=$(id -u)" > .env
+
 # The webserver is available at http://localhost:8080
 # Web URL - http://localhost:5555
-sudo docker compose --profile flower up
-
+sudo docker compose --profile flower up --detach
